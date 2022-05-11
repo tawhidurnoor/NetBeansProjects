35e0e2712d39cde7bc18b131d61bde2fef889c84
Tawhidur Noor <56801563+tawhidurnoor@users.noreply.github.com>
2022-05-11 09:46:11 +0600

    Screenshot Class Update

--- LogWorkClone/src/main/java/com/mycompany/logworkclone/ScreenShot.java
+++ LogWorkClone/src/main/java/com/mycompany/logworkclone/ScreenShot.java
@@ -4,10 +4,67 @@
  */
 package com.mycompany.logworkclone;
 
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.File;
+import javax.imageio.ImageIO;
+
+//for date
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author User
  */
 public class ScreenShot {
     
+    private String email;
+    private String project;
+
+    public ScreenShot(String email, String project) {
+        this.email = email;
+        this.project = project;
+    }
+
+    public static final long serialVersionUID = 1L;
+
+    public void capture() {
+        while (true) {
+
+            try {
+                Thread.sleep(120);
+                Robot r = new Robot();
+
+                // It saves screenshot to desired path
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                SimpleDateFormat sdf3 = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+                String timeStamp = sdf3.format(timestamp);
+                String file_name = timeStamp + "_ScreenShot.jpg";
+                String path = "E:/Tawhidur Nood Badhan/Time_Tracker_Solution--Web-Module/public/captured/"+file_name;
+
+                // Used to get ScreenSize and capture image
+                Rectangle capture = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+                BufferedImage Image = r.createScreenCapture(capture);
+                ImageIO.write(Image, "jpg", new File(path));
+                System.out.println("Screenshot saved");
+                //System.out.println(Screenshot.stopper);
+            } catch (AWTException | IOException | InterruptedException ex) {
+                System.out.println(ex);
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ScreenShot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
 }
